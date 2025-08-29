5faa3f0b2ad29a43047534e391b41e156e0fd3d7
Akuya Ekorot <akuyaekorot@gmail.com>
2025-08-29 15:46:06 +0300

    feat: set up meeting scheduler agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,18 +1,33 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
-import { weatherAgent } from "./agents/weather-agent";
+import { meetingSchedulerAgent } from "./agents/meeting-scheduler";
 
 export const mastra = new Mastra({
-  workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+  agents: { meetingSchedulerAgent },
   storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+    url: "file:../../mastra.db",
   }),
   logger: new PinoLogger({
     name: "Mastra",
     level: "info",
   }),
+  server: {
+    middleware: [{
+      handler: async (c, next) => {
+        const runtimeContext = c.get("runtimeContext");
+
+        // TODO: Retrieve unique user id and set it on the runtime context
+        // Consider using Authentication headers for user identification
+        // e.g const bearerToken = c.get('Authorization')
+        // https://mastra.ai/en/docs/server-db/middleware#common-examples
+        const userId = "unique-user-id";
+
+        runtimeContext.set("userId", userId);
+
+        return next();
+      },
+      path: "/api/*",
+    }],
+  },
 });
